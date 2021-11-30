fd16ede50e6ee6971a53c3efd64ae750986aa3ce
wezerg <55143351+wezerg@users.noreply.github.com>
2021-11-30 12:23:54 +0100

    add climb

--- src/Controller/ClimbController.php
+++ src/Controller/ClimbController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Form\ClimbCreateType;
 use App\Repository\ClimbRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -32,11 +35,28 @@ class ClimbController extends AbstractController
         ]);
     }
 
-    #[Route('/createClimb', name: 'createClimb')]
+    #[Route('/createClimb', name: 'createClimb', methods: "GET")]
     public function creation(): Response
     {
+        $type = ClimbCreateType::class;
+        $form = $this->createForm($type);
         return $this->render('climb/create.html.twig', [
             'controller_name' => 'ClimbController',
+            'form' => $form->createView(),
         ]);
+    }
+
+    #[Route('/createClimb', name: 'createClimbPost', methods: "POST")]
+    public function creationPost(RequestStack $request, ClimbRepository $climbRepo): Response
+    {
+        $type = ClimbCreateType::class;
+        $form = $this->createForm($type);
+        $form->handleRequest($request->getCurrentRequest());
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $user = $this->getUser();
+            $climbRepo->createClimb($user, $data);
+        }
+        return $this->redirectToRoute('listClimb');
     }
 }
